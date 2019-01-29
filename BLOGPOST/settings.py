a7af43d6fdac75a9a545e5809be71046f468f318
Dave Augustus <devpandey19924u@gmail.com>
2019-01-29 21:05:59 +0530

    Added with mysql, register function needs to be fixed

--- BLOGPOST/settings.py
+++ BLOGPOST/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'BLOGPOST.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BLOGPOST',
+        'USER': 'root',
+        'PASSWORD': 'Megamind',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
